cd8cda31b85a0947322b09a5c9c2073124a2162d
ghost-lolade <ghostdeveloper6@gmail.com>
2021-08-26 03:10:37 +0100

    wrote the relationsship functions in the models

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+/**
+     * Get the user that owns the Order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function invoice()
+    {
+        return $this->hasOne(Invoice::class);
+    }
+    public function items()
+    {
+        return $this->belongsToMany(Item::class);
+    }
 }
